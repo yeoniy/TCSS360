c4207bb91b6c62df5b90ef3e57750834fe92bb26
yeoniy <yeonily@uw.edu>
2014-05-27 10:07:05 -0700

    Loader now returns array of Conference instead of String

--- src/util/Loader.java
+++ src/util/Loader.java
@@ -1,38 +1,48 @@
 package util;
 
+import model.Conference;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public final class Loader {
-	
+    private static final int DATE_INDEX = 1;
+	public static int total_conference;
 	public static final String CONFERENCE_LIST_PATH = "Resources\\Conference.txt";
 	
 	public static final String CONFERENCE_PATH = "Resources\\";
 	
 	private Loader() {
+        total_conference = 0;
 		// Should not be instantiated.
 	}
 	
-	public static String[] loadConferenceList() throws FileNotFoundException, IOException {
+	public static Conference[] loadConferenceList() throws FileNotFoundException, IOException {
 		File conferenceFile = new File(CONFERENCE_LIST_PATH);
 		FileReader fr = new FileReader(conferenceFile);
 		BufferedReader br = new BufferedReader(fr);
-		String[] toReturn;
-		ArrayList<String> conferenceList = new ArrayList<String>();
+		Conference[] toReturn;
+		ArrayList<Conference> conferenceList = new ArrayList<Conference>();
 		
 		// Parse the file gathering all the conferences into the list
 		String current;
 		while (br.ready()) {
 			current = br.readLine();
-			conferenceList.add(current);
+            String[] list = current.split(",");
+            String[] tempDate = list[DATE_INDEX].split("/");
+            Date date = new Date(Integer.parseInt(tempDate[0]), Integer.parseInt(tempDate[1]), Integer.parseInt(tempDate[2]));
+            Conference tempConference = new Conference(list[0], date, list[2]);
+			conferenceList.add(tempConference);
 		}
 		br.close();
 		fr.close();
-		toReturn = new String[conferenceList.size()];
+		toReturn = new Conference[conferenceList.size()];
+        total_conference = conferenceList.size();
 		for (int i = 0; i < toReturn.length; i++) {
 			toReturn[i] = conferenceList.get(i);
 		}
