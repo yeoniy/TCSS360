09cec1051f7a263b4ea1063b22598e7145e6e5ac
yeoniy <yeonily@uw.edu>
2014-05-29 11:33:03 -0700

    Added MenuBar to ConferenceGui Frame. I modified LoginController to have get method for User and also LoginPanel has getUser method. However, the exception method has to be handled.
    There's a bit of problem on ConferenceGui constructor where it tends to get user before user Logs in.

--- src/view/ConferenceGui.java
+++ src/view/ConferenceGui.java
@@ -7,7 +7,10 @@ import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import controller.Controller;
+import model.Author;
 import model.Conference;
+import view.menu.MenuBar;
 
 /**
  * Created by Yeonil on 4/29/14.
@@ -27,12 +30,19 @@ public class ConferenceGui extends JFrame {
 	
     private JPanel mainPanel;
     private LoginPanel loginPanel;
+    private MenuBar menuBar;
 
     public ConferenceGui() {
         //mainPanel = new MainPanel();
         loginPanel = new LoginPanel();
         LoginDialog d = new LoginDialog(this);
         mainPanel = new MainPanel(loginPanel.getConference(), loginPanel.getUsername());
+
+        //TODO menuBar creates before User logs in.
+        //menuBar = new MenuBar(new Controller(), new Author(loginPanel.getUser()));
+        //Testing purpose
+        menuBar = new MenuBar(new Controller(), new Author("", loginPanel.getUsername(), ""));
+        setJMenuBar(menuBar);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocation(CENTER.width/2-this.getSize().width/2, CENTER.height/2-this.getSize().height/2);
