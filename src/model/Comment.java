e50295a664edd8440d27428db654ac229816e05a
richard <richard@10.211.55.3>
2014-06-06 11:15:58 -0700

    Comments for paper.java

--- src/model/Comment.java
+++ src/model/Comment.java
@@ -4,11 +4,33 @@ package model;
  * Created by Yeonil on 6/3/14.
  */
 public class Comment {
-
+	/**
+	 * The file where the paper is located
+	 */
     private String file;
+    
+    /**
+     * The id assigned to the paper
+     */
     private String id;
+    
+    /**
+     * The rating assigned to the paper
+     */
     private String rate;
+    
+    /**
+     * Reviewer Comments
+     */
     private String comment;
+    
+    /**
+     * Constructor
+     * @param file The file where the paper is located
+     * @param id The id given to the paper
+     * @param rate The rating given to the paper
+     * @param comment Reviewer comments
+     */
     public Comment(String file, String id, String rate, String comment) {
         this.file = file;
         this.id = id;
@@ -16,18 +38,34 @@ public class Comment {
         this.comment = comment;
     }
 
+    /**
+     * Gets the file
+     * @return the file.
+     */
     public String getFile() {
         return file;
     }
 
+    /**
+     * Gets the ID
+     * @return The id String
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the paper rating.
+     * @return the rating string
+     */
     public String getRate() {
         return rate;
     }
 
+    /**
+     * Gets the reviewer comments
+     * @return the comment string.
+     */
     public String getComment() {
         return comment;
     }
--- src/model/Paper.java
+++ src/model/Paper.java
@@ -17,10 +17,18 @@ import exception.InvalidInputException;
 public class Paper extends Observable {
 	
 	/**
-     * 
+     * Constant representing an unprocessed paper
      */
     private final int PROCESS_PAPER = 0;
+    
+    /**
+     * Constant representing a rejected paper
+     */
     private final int REJECTED_PAPER = 1;
+    
+    /**
+     * Constant representing an accepted paper
+     */
     private final int ACCEPTED_PAPER = 2;
 	
 	/**
@@ -37,10 +45,26 @@ public class Paper extends Observable {
      * int 0=process 1=rejected 2=accepted
      */
     private int accepted;
+    
+    /**
+     * Sub-chair recommendation
+     */
     private int screc;
+    
+    /**
+     * The file name of the paper
+     */
     private String fileName;
+    
+    /**
+     * The ID given to the paper
+     */
     private String id;
-
+    
+    /**
+     *  Constructor
+     * @param file The file where the paper is located
+     */
     public Paper(File file) {
         this.file  = file;
         screc = PROCESS_PAPER;
@@ -50,6 +74,11 @@ public class Paper extends Observable {
         id = "";
     }
     
+    /**
+     * Constructor
+     * @param file The file where the paper is located
+     * @param id The ID given to the paper
+     */
     public Paper(File file, String id) {
         this.file  = file;
         screc = PROCESS_PAPER;
@@ -59,11 +88,18 @@ public class Paper extends Observable {
         this.id = id;
     }
 
-
+    /**
+     * Gets the file name
+     * @return the file name string
+     */
     public String getFileName() {
         return fileName;
     }
     
+    /**
+     * get the file header
+     * @return the file header string
+     */
     public String getFileHeader() {
     	String s = fileName;
     	if(fileName.equals("empty.txt")) {
@@ -72,29 +108,49 @@ public class Paper extends Observable {
         return s;
     }
 
+    /**
+     * set the file name
+     * @param fileName - The name for the paper
+     */
     public void setFileName(String fileName) {
         this.fileName = fileName;
         this.setChanged();
         this.notifyObservers();
     }
 
+    /**
+     * Get the file where the paper is located.
+     * @return the file for the paper.
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * Set the file where the paper is located.
+     * @param file The file where the paper is located.
+     */
     public void setFile(File file) {
         this.file = file;
         this.setChanged();
         this.notifyObservers();
     }
 
+    /**
+     * Return value based on status of paper
+     * @return value based on status of paper
+     */
     public int isAccepted() {
         return accepted;
     }
-    
+    /**
+     * Boolean value based on sub-chair recommendation status.
+     * @return  value based on sub-chair status.
+     */
     public int isScrec() {
         return screc;
     }
+    
     /**
      * Set status of paper
      * @param accepted int to choose status of paper. 0=process 1=rejected 2=accepted
@@ -108,6 +164,18 @@ public class Paper extends Observable {
         this.setChanged();
         this.notifyObservers();
     }
+    
+    /**
+     * Set the Sub-chair recommendation status for a paper
+     * <dt><b>Precondition:</b></dt><dd>
+	 *  Recommendation is within range 0-2
+	 * </dd>
+	 * <dt><b>Postcondition:</b></dt><dd>
+	 * 	Recommendation is set for paper.		
+	 * </dd>
+     * @param accepted int describing status of recommendation
+     * @throws InvalidInputException 
+     */
     public void setSCrec(int accepted) throws InvalidInputException{
         if(accepted >= PROCESS_PAPER && accepted <= ACCEPTED_PAPER)
             this.screc = accepted;
@@ -117,12 +185,20 @@ public class Paper extends Observable {
         this.notifyObservers();
     }
 
+    /**
+     * Add a comment to the paper
+     * @param comment the comment string
+     */
     public void addComment(String comment) {
         this.comment.add(comment);
         this.setChanged();
         this.notifyObservers();
     }
     
+    /**
+     * Test if two papers are equal
+     * @return boolean value indicating equality of papers
+     */
     @Override
     public boolean equals(Object o) {
     	if (o instanceof Paper) {
@@ -134,16 +210,27 @@ public class Paper extends Observable {
     	return false;
     }
 
+    /**
+     * Return string representation of the file name.
+     * @return the file name string.
+     */
     public String toString() {
     	return fileName;
     }
 
 
+    /**
+     * Return the ID associated with the paper
+     * @return The ID string.
+     */
 	public String getId() {
 		return id;
 	}
 
-
+	/**
+	 * Sets the ID for the paper
+	 * @param id The ID string.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
